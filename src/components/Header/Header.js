464fb196bb8ab12a8ddb2f4532b228f63f4d25ee
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-11-25 19:56:47 +0500

    feat: сделать хедер фиксированным

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,34 +5,36 @@ import './Header.css';
 export default function Header({ isInscribed, date, time }) {
   return (
     <header className="header">
-      <Link className="header__logo-container" to="/">
-        <div className="header__logo" />
-        <div>
-          <p className="header__title">BGP&nbsp;AUTO</p>
-          <p className="header__subtitle">АВТОСЕРВИС</p>
-        </div>
-      </Link>
       <div className="header__container">
-        <div className="header__info">
-          <Link className="header__reception" to="/reception">
-            {isInscribed ?
+        <Link className="header__logo-container" to="/">
+          <div className="header__logo" />
+          <div>
+            <p className="header__title">BGP&nbsp;AUTO</p>
+            <p className="header__subtitle">АВТОСЕРВИС</p>
+          </div>
+        </Link>
+        <div className="header__info-container">
+          <div className="header__info">
+            <Link className="header__reception" to="/reception">
+              {isInscribed ?
+                <div>
+                  <p className="header__reception-text">Вы записаны на сервис</p>
+                  <p className="header__reception-text">На: {date && date.split('-').reverse().join('-')} в: {time}</p>
+                </div> :
+                'Записаться на сервис'
+              }
+              <div className={`header__reception-icon ${isInscribed ? 'header__reception-icon_inscribed' : 'header__reception-icon_not-inscribed'}`} />
+            </Link>
+            <Link className="header__address-container" to="/address">
+              <div className="header__address-logo" />
               <div>
-                <p className="header__reception-text">Вы записаны на сервис</p>
-                <p className="header__reception-text">На: {date && date.split('-').reverse().join('-')} в: {time}</p>
-              </div> :
-              'Записаться на сервис'
-            }
-            <div className={`header__reception-icon ${isInscribed ? 'header__reception-icon_inscribed' : 'header__reception-icon_not-inscribed'}`} />
-          </Link>
-          <Link className="header__address-container" to="/address">
-            <div className="header__address-logo" />
-            <div>
-              <p className="header__address-name">Челябинск, Валдайская 17а</p>
-              <p className="header__address-viewing">Посмотреть на карте</p>
-            </div>
-          </Link>
+                <p className="header__address-name">Челябинск, Валдайская 17а</p>
+                <p className="header__address-viewing">Посмотреть на карте</p>
+              </div>
+            </Link>
+          </div>
+          <Navigation />
         </div>
-        <Navigation />
       </div>
     </header>
   )
