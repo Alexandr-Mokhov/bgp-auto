d45f59c57c6a7e0441e45f1eed73845143643ba6
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-11-26 11:52:55 +0500

    feat: сделать футер без ушей

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,11 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer className="footer">
-      <h2 className="footer__copywriting">&copy; {new Date().getFullYear()} BGP AUTO. Александр Мохов</h2>
-      <div className="footer__social">
-        <a href="https://vk.com/bgp.auto" target="_blank" rel="noreferrer"><div className="footer__social-icon" /></a>
+      <div className="footer__container">
+        <h2 className="footer__copywriting">&copy; {new Date().getFullYear()} BGP AUTO. Александр Мохов</h2>
+        <div className="footer__social">
+          <a href="https://vk.com/bgp.auto" target="_blank" rel="noreferrer"><div className="footer__social-icon" /></a>
+        </div>
       </div>
     </footer>
   )
