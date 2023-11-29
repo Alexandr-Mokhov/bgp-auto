b2c2cfa500892f7c76c195747d6a75c787bf5d46
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-11-29 18:36:48 +0500

    refactor: отцентрировать вращающиеся элементы прелоадера

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -5,7 +5,9 @@ export default function Preloader() {
     <div className="preloader">
       <div className="preloader__background" />
       <div className="preloader__container">
-        <div className="preloader__gear preloader__gear_red" />
+        <div className="preloader__container-red-gear">
+          <div className="preloader__gear preloader__gear_red" />
+        </div>
         <div className="preloader__container-black-gear">
           <div className="preloader__gear preloader__gear_black" />
           <div className="preloader__gear preloader__gear_black" />
