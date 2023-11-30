f3549925ee595280b7c644ecdfd8c23fb1a90b82
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-11-30 20:12:08 +0500

    feat: добавить адаптив в Promotions

--- src/components/ListContainer/ListContainer.js
+++ src/components/ListContainer/ListContainer.js
@@ -6,8 +6,8 @@ export default function ListContainer({ list }) {
     return list.map((item, i) => {
       return <li className="list__item" key={i}>
         <img className="list__item-image" src={item.image} alt={item.title} />
+        <h2 className="list__item-title">{item.title}</h2>
         <div className="list__item-description">
-          <h2 className="list__item-title">{item.title}</h2>
           {item.paragraph.map((elem, i) => <p className="list__item-text" key={i}>{elem}</p>)}
         </div>
       </li>
