382311edc7ae64ba0dcd5d5e84bbaaf61ad9a081
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-12-06 21:34:29 +0500

    hotfix: убрать моргание кнопки бургер-меню, упростить нейминг классов

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -28,44 +28,44 @@ export default function Navigation() {
     <div className="navigation">
       <button className={
         `navigation__button-menu
-          ${navMenu ? "navigation__button-menu_type_close" : "navigation__button-menu_type_open"}
+          ${navMenu ? "navigation__button-menu_close" : "navigation__button-menu_open"}
           `} onClick={() => setNavMenu(!navMenu)} />
-      <nav className={`navigation__container ${!navMenu && "navigation__container_type_hide"}`}>
+      <nav className={`navigation__container ${!navMenu && "navigation__container_hide"}`}>
         <ul className="navigation__links">
           <li>
             <NavLink to="/about-as" onClick={resetScroll} className={
-              ({ isActive }) => `navigation__link ${isActive && "navigation__link_type_active"}`
+              ({ isActive }) => `navigation__link ${isActive && "navigation__link_active"}`
             }>О НАС</NavLink>
           </li>
           <li>
             <NavLink to="/services" onClick={resetScroll} className={
-              ({ isActive }) => `navigation__link ${isActive && "navigation__link_type_active"}`
+              ({ isActive }) => `navigation__link ${isActive && "navigation__link_active"}`
             }>УСЛУГИ</NavLink>
           </li>
           <li>
             <NavLink to="/prices" onClick={resetScroll} className={
-              ({ isActive }) => `navigation__link ${isActive && "navigation__link_type_active"}`
+              ({ isActive }) => `navigation__link ${isActive && "navigation__link_active"}`
             }>ПРАЙС</NavLink>
           </li>
           <li>
             <NavLink to="/promotions" onClick={resetScroll} className={
-              ({ isActive }) => `navigation__link ${isActive && "navigation__link_type_active"}`
+              ({ isActive }) => `navigation__link ${isActive && "navigation__link_active"}`
             }>АКЦИИ</NavLink>
           </li>
           <li>
             <NavLink to="/blog" onClick={resetScroll} className={
-              ({ isActive }) => `navigation__link ${isActive && "navigation__link_type_active"}`
+              ({ isActive }) => `navigation__link ${isActive && "navigation__link_active"}`
             }>БЛОГ</NavLink>
           </li>
           <li>
             <NavLink to="/contacts" onClick={resetScroll} className={
-              ({ isActive }) => `navigation__link ${isActive && "navigation__link_type_active"}`
+              ({ isActive }) => `navigation__link ${isActive && "navigation__link_active"}`
             }>КОНТАКТЫ</NavLink>
           </li>
           {navMenu &&
             <li>
               <NavLink to="/address" onClick={resetScroll} className={
-                ({ isActive }) => `navigation__link ${isActive && "navigation__link_type_active"}`
+                ({ isActive }) => `navigation__link ${isActive && "navigation__link_active"}`
               }>АДРЕС</NavLink>
             </li>}
         </ul>
