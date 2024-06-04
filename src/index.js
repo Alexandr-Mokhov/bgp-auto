6e8970cca0a8232239a1551bc8315531bbba04fd
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2024-06-04 20:10:30 +0500

    feat: сделать управление меню навигации и выделение ссылок

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 const body = document.querySelector('.body');
-const menuButton = body.querySelector('.navigation__button-menu');
-const navigationContainer = body.querySelector('.navigation__container');
-const navigationOverlay = body.querySelector('.navigation__overlay');
+const header = body.querySelector('.header');
+const logoContainer = header.querySelector('.header__logo-container');
+const addressContainer = header.querySelector('.header__address-container')
+const navigation = body.querySelector('.navigation');
+const menuButton = navigation.querySelector('.navigation__button-menu');
+const navigationContainer = navigation.querySelector('.navigation__container');
+const navigationOverlay = navigation.querySelector('.navigation__overlay');
+const linksContainer = navigation.querySelector('.navigation__links');
+const navigationLinksList = navigation.querySelectorAll('.navigation__link');
 
-function openMenu() {
+function handleMenu() {
   menuButton.classList.toggle('navigation__button-menu_close');
   navigationContainer.classList.toggle('navigation__container_hide');
   navigationOverlay.classList.toggle('navigation__overlay_hide');
 }
 
-menuButton.addEventListener('click', openMenu);
-navigationOverlay.addEventListener('click', openMenu);
+function disactiveLink() {
+  navigationLinksList.forEach(link => link.classList.remove('navigation__link_active'));
+}
+function paintingLinks(evt) {
+  disactiveLink();
+  evt.target.classList.add('navigation__link_active');
+  
+  const windowWidth = window.innerWidth;
+  if (windowWidth < 850) {
+    handleMenu();
+  }
+}
+
+menuButton.addEventListener('click', handleMenu);
+navigationOverlay.addEventListener('click', handleMenu);
+linksContainer.addEventListener('click', paintingLinks);
+logoContainer.addEventListener('click', disactiveLink);
+addressContainer.addEventListener('click', disactiveLink);
